de983a11850abfeb5f27a2296099f387754971bb
sina <57502518+sinabigo@users.noreply.github.com>
2024-07-09 20:41:57 +0330

    Update hiddifyWarp.php

--- hiddifyWarp.php
+++ hiddifyWarp.php
@@ -40,11 +40,11 @@ $profileConfigs = [
     "warp://" . $choosenIps[1] . ":" . $choosenPort . "?ifp=5-10#WiW-🔵&&detour=warp://" . $choosenIps[0] . ":" . $choosenPort . "?ifp=5-10#WARP-🔵",
 ];
 
-$profileHeader = "#profile-title: base64:" . base64_encode("TVC | WARP") . "
+$profileHeader = "#profile-title: base64:" . base64_encode("SBO | WARP") . "
 #profile-update-interval: 1
 #subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
-#support-url: https://t.me/v2raycollector
-#profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector
+#support-url: https://t.me/sinabigo
+#profile-web-page-url: https://github.com/sinabigo/SBO
 ";
 
 $profileOutput = $profileHeader . "
